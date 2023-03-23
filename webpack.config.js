78c2386db8ec53c107a77db0e692b31164e7ba36
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-03-23 11:01:52 +0300

    Add CSS Loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,17 @@ module.exports = {
     static: './dist',
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     publicPath: '/',
   },
-  optimization: {
-    runtimeChunk: 'single',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
